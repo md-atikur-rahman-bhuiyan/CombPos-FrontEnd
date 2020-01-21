2a60690cd38510c04ef873eb9344683161032aa7
Md Atikur Rahman Bhuiyan <atikbhuiyan.bd@gmail.com>
2020-01-21 19:03:36 +0600

    features panels and hero animation

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 
 
 jQuery(document).ready(function(){
-    jQuery("#menu-handler").click(function(){
-        console.log("Hi");
-    });
+    gsap.to(".hero-icon-light", {duration: 2, y: 200});
+    gsap.to(".hero-icon-light-2", {duration: 5, y: 300});
+    gsap.to("#packages-panel", {duration: 2, scrollTo: {y: 400, x: 200}, ease: "power2"});
 });
