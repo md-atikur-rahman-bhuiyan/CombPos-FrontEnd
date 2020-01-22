1f08a9882cb54193b9636aab00e96aedbef748eb
Md Atikur Rahman Bhuiyan <atikbhuiyan.bd@gmail.com>
2020-01-22 19:10:46 +0600

    Work With Footer Menu and responsive

--- js/custom.js
+++ js/custom.js
@@ -4,4 +4,5 @@ jQuery(document).ready(function(){
     gsap.to(".hero-icon-light", {duration: 2, y: 200});
     gsap.to(".hero-icon-light-2", {duration: 5, y: 300});
     gsap.to("#packages-panel", {duration: 2, scrollTo: {y: 400, x: 200}, ease: "power2"});
+
 });
